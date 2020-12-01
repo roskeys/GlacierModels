f5eba56835c51cf97816a0e1db45d4eed699bf97
AidenKitamura <kokojj123@gmail.com>
2020-12-01 16:53:03 +0800

    bug fixes for resnet 18

--- resnet_18.py
+++ resnet_18.py
@@ -24,48 +24,48 @@ def getModel(name):
     print(x.shape)
 
     # conv1
-    model = Conv2D(x, nb_filter=64, kernel_size=(7, 7), strides=(2, 2), padding="valid")
+    model = Conv2D(filters=4, kernel_size=(3, 3), padding="valid")(x)
     model = BatchNormalization(axis=2)(model)
-    model = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding="same")(model)
+    model = MaxPooling2D(pool_size=(3, 3), padding="same")(model)
 
     # conv2
-    model = ResidualBlock(model, nb_filter=64, kernel_size=(3, 3))
-    model = ResidualBlock(model, nb_filter=64, kernel_size=(3, 3))
+    model = ResidualBlock(model, filters=8, kernel_size=(3, 3))
+    model = ResidualBlock(model, filters=8, kernel_size=(3, 3))
 
     # conv3
-    model = ResidualBlock(model, nb_filter=128, kernel_size=(3, 3))
-    model = ResidualBlock(model, nb_filter=128, kernel_size=(3, 3))
+    model = ResidualBlock(model, filters=16, kernel_size=(3, 3))
+    model = ResidualBlock(model, filters=16, kernel_size=(3, 3))
 
     # conv4
-    model = ResidualBlock(model, nb_filter=256, kernel_size=(3, 3))
-    model = ResidualBlock(model, nb_filter=256, kernel_size=(3, 3))
+    model = ResidualBlock(model, filters=32, kernel_size=(3, 3))
+    model = ResidualBlock(model, filters=32, kernel_size=(3, 3))
 
     # conv5
-    model = ResidualBlock(model, nb_filter=512, kernel_size=(3, 3))
-    model = ResidualBlock(model, nb_filter=512, kernel_size=(3, 3))
+    model = ResidualBlock(model, filters=64, kernel_size=(3, 3))
+    model = ResidualBlock(model, filters=64, kernel_size=(3, 3))
 
     # average
     model = AveragePooling2D(pool_size=(7, 7))(model)
     model = Flatten()(model)
     model = Dense(1)(model)
 
-    m = Model(inputs=[input_x1, input_x2, input_x3, input_x4], outputs=pred, name=name)
+    m = Model(inputs=[input_x1, input_x2, input_x3, input_x4], outputs=model, name=name)
     return m
 
 
 # Defines the Residual Block, revised
-def ResidualBlock(model, nb_filter, kernel_size, strides=(1, 1), padding='same', name=None):
+def ResidualBlock(model, filters, kernel_size, strides=(1, 1), padding='same', name=None):
     bn_name = (name + "_bn") if name != None else None
     conv_name = (name + "_conv") if name != None else None
 
     # BN->ReLU->Conv->BN->ReLU->Conv
     block = BatchNormalization(axis=2, name=bn_name)(model)
     block = ReLU()(block)
-    block = Conv2D(nb_filter, kernel_size, padding=padding, strides=strides, name=conv_name)(block)
+    block = Conv2D(filters, kernel_size, padding=padding, strides=strides, name=conv_name)(block)
 
     block = BatchNormalization(axis=2, name=bn_name)(block)
     block = ReLU()(block)
-    block = Conv2D(nb_filter, kernel_size, padding=padding, strides=strides, name=conv_name)(block)
+    block = Conv2D(filters, kernel_size, padding=padding, strides=strides, name=conv_name)(block)
 
     block = add([model, block])
 
