06fb2b0553ec4e9246282bc9721207d0d3f6826c
roskey <lsjsg@outlook.com>
2021-01-12 11:56:57 +0800

    update with new data

--- train_models.py
+++ train_models.py
@@ -7,47 +7,29 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import importlib
 import pandas as pd
 from utils import train_model
-from utils.load_data import train_test_split, load_data_of_glacier, match_centroid, load_data_by_cluster
+from utils.load_data import train_test_split, load_data_by_cluster
 
+centroid_map = {5: 'AASIAAT(EGEDESMINDE)', 1: 'DANMARKSHAVN', 2: 'ITTOQQORTOORMIIT', 4: 'MITTARFIK_NARSARSUAQ',
+                3: 'TASIILAQ(AMMASSALIK)'}  # 'PITUFFIK',
+
+# get all the models
 model_files = os.listdir("models/basic")
 model_files.remove("__init__.py")
 if "__pycache__" in model_files:
     model_files.remove("__pycache__")
 model_names = [n[:-3] for n in model_files]
 
-for cluster in os.listdir("data/cluster"):
-    x_all, y_all = load_data_by_cluster(f"data/cluster/{cluster}", "data/smb.csv")
+for glacier in pd.read_csv("data/glacier_assignment.csv")["NAME"].unique():
+    x_all, y_all = load_data_by_cluster("data/glacier_assignment.csv", glacier, centroid_map,
+                                        "data/IGRA Archieves/", "data/newDMI8_data",
+                                        "data/smb_mass_change.xlsx")
     if len(y_all) < 25:
         continue
     test_size = int(len(y_all) * 0.2)
     x_train, x_test, y_train, y_test = train_test_split(x_all, y_all, test_size=test_size)
     for module_name in model_names:
         module = importlib.import_module(module_name)
-        model = module.getModel(f"{module_name}_{cluster}")
-        print("#" * 20, "Start to train model: ", f"{module_name}_{cluster}", "#" * 20)
+        model = module.getModel(f"{module_name}_{glacier}")
+        print("#" * 20, "Start to train model: ", f"{module_name}_{glacier}", "#" * 20)
         train_model(model, epoch=2000, data=(x_train, x_test, y_train, y_test),
                     loss='mse', optimizer='rmsprop', save_best_only=True, metrics=['mse'])
-
-# centroid_assignment = pd.read_csv("data/DMI_8_assignment.csv")
-#
-# model_files = os.listdir("models/basic")
-# model_files.remove("__init__.py")
-#
-# if "__pycache__" in model_files:
-#     model_files.remove("__pycache__")
-#
-# model_names = [n[:-3] for n in model_files]
-# for glacier in os.listdir("data/DMI8_data"):
-#     centroid = match_centroid(glacier, centroid_assignment)
-#     x_all, y_all = load_data_of_glacier(f"data/DMI8_data/{glacier}", f"data/IGRA Archieves/{centroid}", "data/smb.csv")
-#     if len(y_all) < 25:
-#         continue
-#     test_size = int(len(y_all) * 0.2)
-#     x_train, x_test, y_train, y_test = train_test_split(x_all, y_all, test_size=test_size)
-#     for module_name in model_names:
-#         module = importlib.import_module(module_name)
-#         centroid = re.sub("\W", "", centroid)
-#         model = module.getModel(f"{module_name}_{glacier}_{centroid}")
-#         print("#" * 20, "Start to train model: ", f"{module_name}_{glacier}_{centroid}", "#" * 20)
-#         train_model(model, epoch=2000, data=(x_train, x_test, y_train, y_test),
-#                     loss='mse', optimizer='rmsprop', save_best_only=True, metrics=['mse'])
--- utils/load_data.py
+++ utils/load_data.py
@@ -6,16 +6,83 @@ import pandas as pd
 index_matcher = re.compile(".*?/(\d{5})/?")
 
 
+def load_1d_data(dataframe):
+    feature_name = dataframe.columns[-1]
+    if "year" not in dataframe.columns or "month" not in dataframe.columns:
+        if "YEAR" in dataframe.columns:
+            dataframe = dataframe.rename(columns={"YEAR": "year"})
+        if "MONTH" in dataframe.columns:
+            dataframe = dataframe.rename(columns={"MONTH": "month"})
+        if dataframe.columns[1] == '0' and dataframe.columns[2] == '1':
+            dataframe = dataframe.rename(columns={'0': "year", '1': "month"}, errors="raise")
+    year_range = dataframe["year"].unique()
+    year_range.sort()
+    output = pd.DataFrame()
+    for year in year_range[:-1]:
+        data = {"year": year + 1}
+        df_current_year = dataframe[dataframe["year"] == year]
+        for i in range(5, 13):
+            data[i] = df_current_year.loc[df_current_year["month"] == i][feature_name].values
+        next_year = year + 1
+        df_next_year = dataframe[dataframe["year"] == next_year]
+        for i in range(1, 5):
+            data[i] = df_next_year.loc[df_next_year["month"] == i][feature_name].values
+        try:
+            df = pd.DataFrame(data)
+        except ValueError as e:
+            if str(e) == "arrays must all be same length":
+                continue
+        output = pd.concat([output, df], ignore_index=True)
+    return output
+
+
+def load_2d_data(dataframe):
+    height = dataframe.shape[0]
+    year_range = [int(year) for year in dataframe.columns.str.split("_").str[0].unique()[2:]]
+    year_range.sort()
+    output_data = pd.DataFrame()
+    for year in year_range[:-2]:
+        data = {"year": np.array([year + 1] * height)}
+        df_current_year = dataframe.filter(regex=f"^{year}", axis=1)
+        for i in range(5, 13):
+            data[i] = df_current_year.filter(regex=f"_{i}$", axis=1).values.squeeze()
+        next_year = year + 1
+        df_next_year = dataframe.filter(regex=f"^{next_year}", axis=1)
+        for i in range(1, 5):
+            data[i] = df_next_year.filter(regex=f"_{i}$", axis=1).values.squeeze()
+        data['height'] = np.arange(0, height) * 100
+        try:
+            df = pd.DataFrame(data)
+        except ValueError as e:
+            if str(e) == "arrays must all be same length":
+                continue
+            elif str(e) == "Data must be 1-dimensional":
+                continue
+        output_data = pd.concat([output_data, df], ignore_index=True)
+    return output_data
+
+
+def load_smb(name, df):
+    df = df[df["NAME"] == name]
+    columns = df.filter(regex="\d{4}\.5").columns
+    columns = [int(year) for year in columns]
+    sorted(columns)
+    smb = pd.DataFrame()
+    for year in columns:
+        smb = pd.concat([smb, pd.DataFrame({"year": year, "SMB": df[year + 0.5].values})])
+    return smb
+
+
 def load_1d_array(df, year_range):
     df = df[df['year'].isin(year_range)]
-    return df[[str(i) for i in range(1, 13)]].values
+    return df[[i for i in range(1, 13)]].values
 
 
 def load_2d_array(df, year_range):
     df = df[df['year'].isin(year_range)]
     data = []
     for year in df["year"].unique():
-        data.append(df[df["year"] == year][[str(i) for i in range(1, 13)]].values)
+        data.append(df[df["year"] == year][[i for i in range(1, 13)]].values)
     return np.array(data)
 
 
@@ -26,25 +93,6 @@ def load_smb_array(df, year_range):
 
 def get_common_year_range(year_range, df):
     return year_range.intersection(set(df["year"]))
-
-
-def load_data(*paths):
-    dataFrames = []
-    for path in paths:
-        dataFrames.append(pd.read_csv(path))
-    year_range = set(dataFrames[0]["year"])
-    for df in dataFrames:
-        year_range = get_common_year_range(year_range, df)
-    data = []
-    smb = None
-    for df in dataFrames:
-        if df.shape[1] == 15:
-            data.append(np.expand_dims(load_2d_array(df, year_range), axis=-1).astype(np.float))
-        elif df.shape[1] == 14:
-            data.append(load_1d_array(df, year_range).astype(np.float))
-        elif df.shape[1] == 3:
-            smb = load_smb_array(df, year_range).astype(np.float)
-    return data, smb
 
 
 def train_test_split(x, y, test_size=7, random_seed=None):
@@ -64,43 +112,6 @@ def train_test_split(x, y, test_size=7, random_seed=None):
     return x_train, x_test, y_train, y_test
 
 
-def match_centroid(glacier_index, centroid_assignment):
-    centroid_map = {5: 'AASIAAT(EGEDESMINDE)', 1: 'DANMARKSHAVN', 2: 'ITTOQQORTOORMIIT',
-                    4: 'MITTARFIK_NARSARSUAQ', 3: 'TASIILAQ(AMMASSALIK)'}  # 'PITUFFIK',
-    line = centroid_assignment[centroid_assignment["NAME"].str.contains(glacier_index)]
-    if len(line) > 1:
-        line = line.head(1)
-    elif len(line) == 0:
-        return
-    return centroid_map[line["Central"].values[0]]
-
-
-def load_data_of_glacier(dmi8_path, igra_path, smb_path):
-    x, y = load_data(*[
-        smb_path,
-        os.path.join(dmi8_path, "cloud.csv"),
-        os.path.join(dmi8_path, "precipitation.csv"),
-        os.path.join(dmi8_path, "wind.csv"),
-        os.path.join(igra_path, "humidity.csv"),
-        os.path.join(igra_path, "pressure.csv"),
-        os.path.join(igra_path, "temperature.csv"),
-    ])
-    return x, y
-
-
-def load_data_by_cluster(cluster_path, smb_path):
-    x, y = load_data(*[
-        smb_path,
-        os.path.join(cluster_path, "cloud.csv"),
-        os.path.join(cluster_path, "precipitation.csv"),
-        os.path.join(cluster_path, "wind.csv"),
-        os.path.join(cluster_path, "humidity.csv"),
-        os.path.join(cluster_path, "pressure.csv"),
-        os.path.join(cluster_path, "temperature.csv"),
-    ])
-    return x, y
-
-
 def concatenate_data(x1, y1, x2, y2):
     x_concatenated = []
     if isinstance(x1, list) or isinstance(x1, tuple):
@@ -112,13 +123,43 @@ def concatenate_data(x1, y1, x2, y2):
     return x_concatenated, y_concatenated
 
 
+def load_data_by_cluster(glacier_assignment_path, glacier_name, centroid_to_igra_map, igra_base_path, dmi_base_path,
+                         smb_path):
+    smb_df = load_smb(glacier_name, pd.read_excel(smb_path))
+    glacier_assignment = pd.read_csv(glacier_assignment_path)
+    central = glacier_assignment[glacier_assignment['NAME'] == glacier_name]['Central'].values[0]
+    igra_name = centroid_to_igra_map[central]
+    humidity_df = load_2d_data(pd.read_csv(os.path.join(igra_base_path, igra_name, "CalHum_std.csv")))
+    pressure_df = load_2d_data(pd.read_csv(os.path.join(igra_base_path, igra_name, "Pressure.csv")))
+    temperature_df = load_2d_data(pd.read_csv(os.path.join(igra_base_path, igra_name, "Temp.csv")))
+    cloud_df = load_1d_data(pd.read_csv(os.path.join(dmi_base_path, str(central), f"mean_cloud_{central}.csv")))
+    wind_df = load_1d_data(pd.read_csv(os.path.join(dmi_base_path, str(central), f"mean_wind_{central}.csv")))
+    precipitation_df = load_1d_data(
+        pd.read_csv(os.path.join(dmi_base_path, str(central), f"monthly_total_precipitation_{central}.csv")))
+    dataframes = [smb_df, humidity_df, pressure_df, temperature_df, cloud_df, wind_df, precipitation_df]
+    common_year_range = set(smb_df["year"])
+    for df in dataframes[1:]:
+        common_year_range = get_common_year_range(common_year_range, df)
+    smb_array = load_smb_array(smb_df, common_year_range)
+    x_data_set = [
+        load_1d_array(cloud_df, common_year_range),
+        load_1d_array(precipitation_df, common_year_range),
+        load_1d_array(wind_df, common_year_range),
+        load_2d_array(humidity_df, common_year_range),
+        load_2d_array(pressure_df, common_year_range),
+        load_2d_array(temperature_df, common_year_range)
+    ]
+    return x_data_set, smb_array
+
+
 if __name__ == "__main__":
-    all_x, all_y = load_data(*[
-        "../data/MITTARFIK NARSARSUAQ/smb.csv",
-        "../data/MITTARFIK NARSARSUAQ/mean_cloud.csv",
-        "../data/MITTARFIK NARSARSUAQ/mean_precipitation.csv",
-        "../data/MITTARFIK NARSARSUAQ/mean_wind.csv",
-        "../data/MITTARFIK NARSARSUAQ/mean_temperature.csv",
-        "../data/MITTARFIK NARSARSUAQ/mean_humidity.csv",
-        "../data/MITTARFIK NARSARSUAQ/mean_pressure.csv"])
-    x_train, x_test, y_train, y_test = train_test_split(all_x, all_y)
+    centroid_map = {5: 'AASIAAT(EGEDESMINDE)', 1: 'DANMARKSHAVN', 2: 'ITTOQQORTOORMIIT', 4: 'MITTARFIK_NARSARSUAQ',
+                    3: 'TASIILAQ(AMMASSALIK)'}  # 'PITUFFIK',
+
+    x_all, y_all = load_data_by_cluster("../data/glacier_assignment.csv", "ILORLIIT-SERMINNGUAQ", centroid_map,
+                                        "../data/IGRA Archieves/", "../data/newDMI8_data",
+                                        "../data/smb_mass_change.xlsx")
+
+    for x in x_all:
+        print(x.shape)
+    print(y_all.shape)
